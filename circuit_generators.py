9930f61d63960469b0f3bc4ebb228c3e9f212378
jordanaq <quinn.jordan@protonmail.ch>
2025-05-06 20:21:41 -0400

    Cirtuit to graph where applicable

--- circuit_generators.py
+++ circuit_generators.py
@@ -3,10 +3,9 @@ import pyzx as zx
 import pyzx.generate as zxg
 
 
-
-# def gen_cnot_had_phase(n_qubits, n_gates):
-#     graph = zxg.CNOT_HAD_PHASE_circuit(n_qubits, n_gates)
-#     return graph
+def gen_cnot_had_phase(n_qubits, n_gates):
+    c = zxg.CNOT_HAD_PHASE_circuit(n_qubits, n_gates)
+    return c.to_graph()
 
 
 def gen_clifford_t(n_qubits, n_gates):
@@ -36,30 +35,30 @@ def gen_identity(n_qubits, n_gates):
 
 def gen_phase_poly(n_qubits, n_gates):
     n_layers = random.randint(1, min(n_gates, 10))
-    graph = zxg.phase_poly(n_qubits, n_layers, n_gates // n_layers)
-    return graph
+    c = zxg.phase_poly(n_qubits, n_layers, n_gates // n_layers)
+    return c.to_graph()
 
 
 def gen_phase_approx(n_qubits, n_gates):
     n_layers = random.randint(1, min(n_gates, 10))
-    graph = zxg.phase_poly_approximate(n_qubits, n_gates, n_layers)
-    return graph
+    c = zxg.phase_poly_approximate(n_qubits, n_gates, n_layers)
+    return c.to_graph()
 
 
 def gen_phase_from_gadgets(n_qubits, n_gates):
     n_gadgets = random.randint(1, min(n_gates, 10))
-    graph = zxg.phase_poly_approximate(n_qubits, n_gadgets)
-    return graph
+    c = zxg.phase_poly_from_gadgets(n_qubits, n_gadgets)
+    return c.to_graph()
 
 
 def gen_qft(n_qubits, n_gates):
-    graph = zxg.qft(n_qubits)
-    return graph
+    c = zxg.qft(n_qubits)
+    return c.to_graph()
 
 
 def gen_default_circuit_options():
     return {
-        # "CNOT-HAD-PHASE": gen_cnot_had_phase,
+        "CNOT-HAD-PHASE": gen_cnot_had_phase,
         "Clifford + T": gen_clifford_t,
         "Clifford + T + Measurements": gen_clifford_t_meas,
         "Clifford": gen_cliffords,
@@ -67,6 +66,6 @@ def gen_default_circuit_options():
         "Identity": gen_identity,
         "Phase Polynomial": gen_phase_poly,
         "Phase Polynomial Approximate": gen_phase_approx,
-        "Phase Polynomial from Gadgets": gen_phase_from_gadgets,
+        # "Phase Polynomial from Gadgets": gen_phase_from_gadgets,
         "Quantum Fourier Transform (QFT)": gen_qft,
     }
